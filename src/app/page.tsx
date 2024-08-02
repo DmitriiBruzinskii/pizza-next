12514599a7190ba5f562510458952b6461097aa4
dmitrii bruzinskii <bruzinschi_dmitrii@mail.ru>
2024-08-02 13:06:40 +0300

    added sorting for products

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
-import { Categories } from "@/components/shared/categories";
 import { Container } from "@/components/shared/container";
 import { Title } from "@/components/shared/title";
+import { TopBar } from "@/components/shared/topBar";
 
 const Home = () => {
   return (
     <>
       <Container className='mt-10'>
         <Title text='Все пиццы' size='lg' className='font-extrabold' />
-        <Categories />
       </Container>
+      <TopBar />
     </>
   )
 }
