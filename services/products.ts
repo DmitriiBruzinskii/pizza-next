72e63de1969e71625dbc46307686c998e1d65d43
dmitrii bruzinskii <bruzinschi_dmitrii@mail.ru>
2024-09-28 21:16:15 +0300

    added inter variables

--- services/products.ts
+++ services/products.ts
@@ -1,8 +1,9 @@
 import { axiosInstance } from './axiosInstance';
 import { Product } from "@prisma/client";
+import { ApiRoutes } from './constants';
 
 export const search = async (query: string) => {
-  const { data } = await axiosInstance.get<Product>('/products/search', {params: { query }});
+  const { data } = await axiosInstance.get<Product[]>(ApiRoutes.SEARCH_PRODUCTS, {params: { query } });
 
   return data;
 };
