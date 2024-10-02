db4383597019b3eab0b81f1c1de9dffb4847c89e
dmitrii bruzinskii <bruzinschi_dmitrii@mail.ru>
2024-10-02 17:16:52 +0300

    added skeleton when loading from database products

--- hooks/useFilterIngredients.ts
+++ hooks/useFilterIngredients.ts
@@ -4,17 +4,22 @@ import { Api } from '../services/apiClients';
 
 interface ReturnProps {
   ingredients: Ingredient[];
+  loading: boolean;
 }
 
 export const useFilterIngredients = (): ReturnProps => {
   const [ingredients, setIngredients] = useState<Ingredient[]>([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
+    setLoading(true);
+    
     Api.ingredients
       .getAll()
-      .then(ingredients => setIngredients(ingredients))
-      .catch(error => console.log(error));
+      .then((ingredients) => setIngredients(ingredients))
+      .catch((error) => console.log(error))
+      .finally(() => setLoading(false));
   }, []);
 
-  return { ingredients };
+  return { ingredients, loading };
 }
--- src/components/shared/checkboxFiltersGroup.tsx
+++ src/components/shared/checkboxFiltersGroup.tsx
@@ -2,7 +2,7 @@
 
 import React, { useState } from 'react';
 import { FilterCheckbox, FilterChecboxProps } from './FilterCheckbox';
-import { Input } from '../ui';
+import { Input, Skeleton } from '../ui';
 
 type Item = FilterChecboxProps;
 
@@ -13,6 +13,7 @@ interface Props {
   limit?: number;
   searchInputPlaceholder?: string;
   className?: string;
+  loading?: boolean;
   onChange?: (values: string[]) => void;
   defaultValue?: string[];
 }
@@ -24,19 +25,36 @@ export const CheckboxFiltersGroup: React.FC<Props> = ({
   limit = 5,
   searchInputPlaceholder = 'Поиск...',
   className,
+  loading,
   onChange,
   defaultValue,
 }) => {
   const [showAll, setShowAll] = useState(false);
   const [searchValue, setSearchValue] = useState('');
+
   const list = showAll ? items.filter(item => item.text.toLowerCase().includes(searchValue.toLocaleLowerCase())) : defaultItems.slice(0, limit);
+
+  const onChangeSearchInput = (e: React.ChangeEvent<HTMLInputElement>) => setSearchValue(e.target.value);
+
+  if (loading) {
+    return (
+      <div className={className}>
+        <p className='font-bold mb-3'>{title}</p>
+
+        {...Array(limit)
+          .fill(0)
+          .map((_, index) => <Skeleton key={index} className='h-6 mb-4 rounded-[6px]' />)
+        }
+      </div>
+    )
+  }
 
   return (
     <div className={className}>
       <p className='font-bold mb-3'>{title}</p>
       {showAll && (
         <div className='mb-5'>
-          <Input onChange={e => setSearchValue(e.target.value)} placeholder={searchInputPlaceholder} className='bg-gray-50 border-none' />
+          <Input onChange={onChangeSearchInput} placeholder={searchInputPlaceholder} className='bg-gray-50 border-none' />
         </div>
       )}
       <div className='flex flex-col gap-4 max-h-96 pr-2 overflow-auto scrollbar'>
