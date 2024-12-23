c69e50b7997774c687ec2d9e301ac60a38f28869
dmitrii bruzinskii <bruzinschi_dmitrii@mail.ru>
2024-12-23 11:37:53 +0200

    added adding the product and pizza to the basket

--- src/components/shared/ChooseProductForm.tsx
+++ src/components/shared/ChooseProductForm.tsx
@@ -6,14 +6,12 @@ import { Title } from "./Title";
 interface Props {
   name: string,
   imageUrl: string;
+  onSubmit?: VoidFunction,
+  price: number,
   className?: string;
 };
 
-export const ChooseProductForm: React.FC<Props> = ({
-  name,
-  imageUrl,
-  className
-}) => {
+export const ChooseProductForm: React.FC<Props> = ({ name, imageUrl, onSubmit, price, className }) => {
   return (
     <div className={cn(className, 'flex flex-1')}>
       <div className="flex items-center justify-center flex-1 relative w-full">
@@ -27,8 +25,8 @@ export const ChooseProductForm: React.FC<Props> = ({
       <div className="w-[490px] bg-[#f7f6f5] p-7">
         <Title text={name} size="md" className="font-extrabold mb-1" />
 
-        <Button className="h-[55px] px-10 text-base rounded-[18px] w-full mt-10">
-          Добавить в корзину за 300 ₽
+        <Button onClick={onSubmit} className="h-[55px] px-10 text-base rounded-[18px] w-full mt-10">
+          Добавить в корзину за {price} ₽
         </Button>
       </div>
     </div>
--- src/components/shared/modals/ChooseProductModal.tsx
+++ src/components/shared/modals/ChooseProductModal.tsx
@@ -8,6 +8,7 @@ import { ChooseProductForm } from "../ChooseProductForm";
 import { ProductWithRelations } from "../../../../@types/prisma";
 import { ChoosePizzaForm } from "../ChoosePizzaForm";
 import { VisuallyHidden } from "@radix-ui/react-visually-hidden";
+import { useCartStore } from "../../../../store/cart";
 
 interface Props {
   product: ProductWithRelations;
@@ -16,19 +17,24 @@ interface Props {
 
 export const ChooseProductModal: React.FC<Props> = ({ product, className }) => {
   const router = useRouter();
+  const firstItem = product.items[0];
   const isPizzaForm = Boolean(product.items[0].pizzaType);
+  const addCartItem = useCartStore(state => state.addCartItem);
+
+  const onAddProduct = () => addCartItem({ productItemId: firstItem.id });
+  const onAddPizza = (productItemId: number, ingredients: number[]) => addCartItem({ productItemId, ingredients });
 
   return (
     <Dialog open={Boolean(product)} onOpenChange={() => router.back()} >
-      <DialogContent aria-describedby={undefined} className={cn(className, 'p-0 w-[1060px] max-w-[1060px] min-h-[500px] bg-white overflow-hidden')}>
+      <DialogContent aria-describedby='dialog-description' className={cn(className, 'p-0 w-[1060px] max-w-[1060px] min-h-[500px] bg-white overflow-hidden')}>
         <VisuallyHidden>
-          <DialogTitle>Product</DialogTitle>
+          <DialogTitle>Product details</DialogTitle>
         </VisuallyHidden>
 
         {
           isPizzaForm ? (
-            <ChoosePizzaForm imageUrl={product.imageUrl} name={product.name} ingredients={product.ingredients} items={product.items}/>
-          ) : <ChooseProductForm imageUrl={product.imageUrl} name={product.name} />
+            <ChoosePizzaForm imageUrl={product.imageUrl} name={product.name} ingredients={product.ingredients} items={product.items} onSubmit={onAddPizza} />
+          ) : <ChooseProductForm imageUrl={product.imageUrl} name={product.name} onSubmit={onAddProduct} price={firstItem.price}/>
         }
       </DialogContent>
     </Dialog>
